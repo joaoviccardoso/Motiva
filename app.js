891580632ebf7edd19429b3dce9f9c9f3495017b
seu nome de usuário <pealq63@gmail.com>
2025-07-21 13:15:17 -0300

    adicionado um tutorial quanto a pessoa entra na pagina

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ import CarregarMensagem from "./src/js/controller/CarregarMensagem.js";
 import { BtnController } from "./src/js/controller/BtnController.js";
 import { PaletaView } from "./src/js/view/paletaView.js";
 import Filtros from "./src/js/controller/filtroController.js";
+import { modal } from "./src/js/view/modal.js";
 
 const BtnControlle = new BtnController()
 const GerarMensagens = new CarregarMensagem()
 const paleta = new PaletaView()
 const filtro = new Filtros()
 
+const tutorial = localStorage.getItem("tutorial") || "";
 const temaSalvo = localStorage.getItem("tema");
+
 if (temaSalvo) {
   paleta.mudarCorTema(temaSalvo);
 }
+
+console.log(tutorial, temaSalvo)
+if (tutorial === "") {
+  modal.mostrarDialogTutorial()
+} 
 
 GerarMensagens.pegarTodasAsMensagens()
 BtnControlle.garregarPaletaCor()
--- src/js/view/modal.js
+++ src/js/view/modal.js
@@ -17,6 +17,53 @@ export const modal = {
             document.getElementById("closeDialog").addEventListener("click", () => this.fecharDialog(dialog))
     },
 
+    mostrarDialogTutorial(){
+        const dialog = document.getElementById("myDialog");
+
+        dialog.innerHTML = `
+                <h2 class="avisoModal">Como usar o Expressa?</h2>
+                <p class="mensagemModal">Aqui vai um mini tutorial:</p>
+                <div class="container-tutorial-btn">
+                    <span class="circulo-tutorial">
+                        <img class="img-tutorial" src="./src/img/house 1.png" alt="Ícone de Início">
+                    </span>
+                    <p>Home: Leva você de volta à tela inicial para visualizar mensagens e navegar por categorias.</p>
+                </div>
+
+                <div class="container-tutorial-btn">
+                    <span class="circulo-tutorial">
+                        <img class="img-tutorial" src="./src/img/note 1.png" alt="Ícone para cadastrar mensagem">
+                    </span>
+                    <p>Cadastrar mensagem: Aqui você pode escrever e publicar sua própria mensagem para o mural.</p>
+                </div>
+
+                <div class="container-tutorial-btn">
+                    <span class="circulo-tutorial">
+                        <img class="img-tutorial" src="./src/img/eyedropper 1.png" alt="Ícone de tema">
+                    </span>
+                    <p>Paleta de cores: Personalize o visual do app escolhendo a cor que mais combina com você.</p>
+                </div>
+
+                <div class="container-tutorial-btn">
+                    <span class="circulo-tutorial">
+                        <img class="img-tutorial" src="./src/img/Vector.png" alt="Ícone sobre o projeto">
+                    </span>
+                    <p>Sobre: Veja mais informações sobre o projeto e a proposta do Motiva.</p>
+                </div>
+                
+                <div>
+                    <button class="btn-ignorar" id="closeDialog" >Fechar</button>
+                </div>    
+            `
+        
+            dialog.showModal();
+            requestAnimationFrame(() => {
+                dialog.style.opacity = "1";
+                dialog.style.transform = "scale(1)";
+            });
+            document.getElementById("closeDialog").addEventListener("click", () => this.fecharDialogTutorial(dialog))
+    },
+
     fecharDialog(dialog){
         dialog.style.opacity = "0";
         dialog.style.transform = "scale(0.8)";
@@ -25,6 +72,17 @@ export const modal = {
         setTimeout(() => {
             dialog.close();
             location.reload();
+        }, 300);
+    },
+
+    fecharDialogTutorial(dialog){
+        const ignorarTutorial = dialog.id;
+        localStorage.setItem("tutorial", ignorarTutorial);
+        dialog.style.opacity = "0";
+        dialog.style.transform = "scale(0.8)";
+
+        setTimeout(() => {
+            dialog.close();
         }, 300);
     }
 }
